c49843d0cf6b7b8beb2e19d3edd96bde84a4c794
blade0 <blade0@blade0.net>
2026-06-27 19:39:34 +0100

    fix nimbus url (im such a fucking dumbass)

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
     vicinae.url = "github:vicinaehq/vicinae";
     vicinae-extensions.url = "github:vicinaehq/extensions";
-    nimbus.url = "github/Blade04208/nimbus";
+    nimbus.url = "github:Blade04208/nimbus";
   };
   outputs =
     {
